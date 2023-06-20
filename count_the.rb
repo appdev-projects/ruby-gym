c062212cd7c9cc9c6f973cd3b3bdeb8dc6866987
Ben Purinton <ben.purinton@gmail.com>
2023-06-19 21:20:04 -0800

    stuck on count_the spec

--- count_the.rb
+++ count_the.rb
@@ -1,6 +1,21 @@
 # Write a program that: 
-#   asks the user to enter a sentence.
-#   then finds the number of times 'the' appears in the given string
-#   and finally prints, "'the' appeared x times", where x is an Integer
+# Takes a String, then finds the number of times 'the' appears in the given String, and finally prints:
+#
+#   "'the' appeared x times"
+#
+# where x is an Integer.
+#
+# Use the "sentence" variables below to test your code by uncommenting each sentence and trying to use it
+#
+# Hint: It may be helpful to use the advanced gsub techniques with the Regexp class; see the Ruby reference: 
+#   https://learn.firstdraft.com/lessons/33#advanced-gsub-techniques
 
-p "Enter a sentence:"
+# try me 
+sentence = "the dog, the cat, the zebra, the giraffe" # x = 4
+
+# then make sure these work as well!
+# sentence = "the, the code, and the developer" # x = 3
+# sentence = "then the- their" # x = 1
+
+# write your code below
+
